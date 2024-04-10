955d9ac7ca4e16f25df8fbec9d762aa27855c5c2
stoplight@melting-point.fr <stoplight@melting-point.fr>
2024-04-10 09:19:12 +0200

    Added 52 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0Mjc1MQ/spectral.js?branch_slug=master&token=ef863fe6-354c-4fd6-b1cf-e700e2ee253e";
+export default { extends: ruleset };
